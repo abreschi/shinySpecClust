d0434ce8b37584b16d252e15a10268681bb5e75a
abreschi <ale.breschi@gmail.com>
2018-03-22 16:01:58 -0700

    added sample file; remove spinner

--- app.R
+++ app.R
@@ -67,16 +67,50 @@ spinnerUI = tags$img(src="loading_spinner.gif",
 
 ui <- fluidPage(
 
+#	tags$head(HTML("<link rel='stylesheet' src='normalize.css'>")),
+	tags$head(tags$style(HTML("
+		* {
+			font-size: 20px;
+		}
+		p {
+			text-align: justify;
+		}
+		h1 {
+			font-size: 4rem;
+		}
+		body {
+			margin-left: 2rem;
+		}
+	"))),
 
-	# App title ----
-	titlePanel("CGM viewer"),
+	title = "CGM viewer",
+	HTML("
+		<div style='display: flex'> 
+			<h1 style='align-self: center; flex-grow: 1'>CGM viewer</h1>
+			<img style='max-width:250px; min-width:200px; margin: 3rem' 
+				src='Stanford_Medicine_logo-web-CS.trim.png' alt='logo'>
+		</div>
+	"),
+
 	
-	br(),
-
 	# Controls
 	fluidRow(
 
-		column(4,
+		column(4, 
+			HTML("<p>
+			Visualize your CGM profile and discover your glucotype! 
+			This webpage provides an interface to the glucotype classification
+			described in <a href='#'>Hall et al</a>. 
+			To find out about your glucotype please upload a tab-delimited
+			file with two columns, containing the time of measurement and
+			the glucose concentration, following the format in 
+			<a href='sample.cgm.tsv' target='_blank'>this sample file</a>.
+			</p> 
+			")
+		),
+		
+		column(3,
+			style="margin-left: 4rem;",
 			fileInput(inputId = "cgmF", 
 				"Upload CGM profile",
 				multiple = TRUE,
@@ -84,25 +118,36 @@ ui <- fluidPage(
 					"text/comma-separated-values,text/plain",
 					".csv", ".tsv", ".txt"
 				)
+			),
+
+			tags$label("or    "),
+			
+			actionButton(inputId = "showSample",
+				"Visualize sample profile"
 			)
 		),
 
-		column(4,
+		column(2, 
+
 			actionButton(inputId = "glucotype",
 				"Classify glucotype"
 			),
 			actionButton(inputId = "reset",
 				"Clear"
-			)
+			),
+			br(),
+			uiOutput(outputId = "verdict")
 		)	
 	),
 	
 	fluidRow(
 		column(10,
 			tags$div(id = "dygraphContainer",
-				style = "position: relative; text-align: center",
+				style = "position: relative; text-align: center; margin: 1rem auto;",
 				tags$div(
-					dygraphOutput(outputId = "cgmProfile")
+					dygraphOutput(outputId = "cgmProfile"),
+					br(),
+					uiOutput(outputId = "dygraphDesc")
 				)
 			)
 		),
@@ -134,55 +179,82 @@ ui <- fluidPage(
 # ~~~~~~~~~~~~~~~~
 # Define server logic
 server <- function(input, output, session) {
-	
 
-	read_cgmF = reactive ({
-		req(input[['cgmF']]);
-		df = read.csv(input$cgmF$datapath,
+	print("Restart")
+
+	# Read 	
+	read_cgmF = function(f) {
+		df = read.csv(f,
 			header = TRUE,
 			sep = "\t",
 			quote = ""
 		)
 		df[[1]] = ymd_hms(df[[1]])
-		return(df)
+		return (df)
+	}
+
+	v = reactiveValues(data=NULL);
+
+	observeEvent(input$cgmF, {
+		req(input[['cgmF']]);
+		v$data = read_cgmF(input$cgmF$datapath)
+		print(head(v$data))
 	})
+
+	observeEvent(input[["showSample"]], {
+		v$data = read_cgmF("www/sample.cgm.tsv")
+		print(head(v$data))
+	})
+
 
 	# table of raw cgm values
 	output[['cgmTable']] = DT::renderDataTable({
-		DT::datatable(read_cgmF(), 
+		DT::datatable(v$data, 
 			options = list())
 	})
 
 	# dygraph of raw cgm values
 	dygraph_cgm = renderDygraph({
-		df = read_cgmF()
-		print(head(df))
-		dygraph(df_to_xts(df)) %>%
-		dyAxis("y", label = axisTitle)
+		if (is.null(v$data)) return()
+		df = v$data
+		dygraph(df_to_xts(df), ylab=axisTitle) %>%
+		dyOptions(axisLabelWidth=90)
 	})
-	output[["cgmProfile"]] = dygraph_cgm;
+	output[["cgmProfile"]] = dygraph_cgm
+	# Description for dygraph navigation commands
+	output[["dygraphDesc"]] = renderUI({
+		if (is.null(v$data)) return()
+		tags$p("Zoom: click-drag; Pan: shift-click-drag; 
+		Restore zoom level: double-click", 
+		style="text-align: center; color: grey")
+	})
 
-	observeEvent(input$cgmF, {
-		output[["cgmProfile"]] = dygraph_cgm
-		output[["glucotypeTable"]] = renderTable({})
-	})
 
 
 	# Calculate glucotype
 	observeEvent(input$glucotype, {
-		cgm = read_cgmF();
-		insertUI("#dygraphContainer", where="afterBegin", 
-			tags$div(id="spinnerBg", style="position:absolute;
+#		cgm = data();
+		cgm = v$data
+		print("Compute glucotype")
+		print(head(cgm))
+		insertUI("#dygraphContainer", where="afterBegin",
+			tags$div("Computing.... Please be patient", id="spinnerBg", 
+				style="position: absolute; vertical-align: middle;
 				background-color:rgba(200,200,200,0.4); 
 				z-index:10; width:100%; height:100%"), 
 			immediate=TRUE, session=session);
-		insertUI("#dygraphContainer", where="afterBegin", 
-			spinnerUI, immediate=TRUE, session=session);
+#		insertUI("#dygraphContainer", where="afterBegin", 
+#			tags$div(id="spinnerBg", style="position:absolute;
+#				background-color:rgba(200,200,200,0.4); 
+#				z-index:10; width:100%; height:100%"), 
+#			immediate=TRUE, session=session);
+#		insertUI("#dygraphContainer", where="afterBegin", 
+#			spinnerUI, immediate=TRUE, session=session);
 
 		cachedF = "glucotypes.df.tsv"
 		if (!file.exists(cachedF)) {
 			#print(head(preprocess_cgm(cgm)));
-			df = classify_windows(cgm, train_windows, param_list)
+			df = classify_windows(cgm, train_windows, param_list, 0.25)
 #			Have to think of a better way to cache dataframe
 #			write.zoo(df, file = cachedF, quote=F, sep='\t')
 		} else {
@@ -190,7 +262,7 @@ server <- function(input, output, session) {
 			df = as.xts.data.table(df[,Index:=ymd_hms(Index)])
 		}
 		# remove loading spinner at the end of computation
-		observe({removeUI("#spinner"); })
+#		observe({removeUI("#spinner"); })
 		observe({removeUI("#spinnerBg"); })
 		# Plot colored dygraph
 		output[["cgmProfile"]] = renderDygraph({
@@ -198,13 +270,21 @@ server <- function(input, output, session) {
 			print(head(df))
 			dygraph(df) %>%
 			dyAxis("y", label = axisTitle) %>%
+			dyOptions(axisLabelWidth=90) %>%
 			dyOptions(colors=plot_colors, drawPoints=T, pointSize=2)
 		})
 		# Table with glucotype frequencies
+		freq = data.frame("Fraction of time" = 
+				colMeans(!is.na(df)), check.names=F)
 		output[["glucotypeTable"]] = renderTable(
 			rownames = TRUE, {
-			data.frame("Fraction of time" = 
-				colMeans(!is.na(df)), check.names=F)
+			freq
+		})
+		# Print glucotype on top of the page
+		output[["verdict"]] = renderUI({
+			gt = rownames(freq)[which.max(freq[[1]])]
+			gt_color = rev(hue_pal()(3))[match(gt, glucotypes)]
+			h1(gt, style=sprintf("color: %s", gt_color))
 		})
 	})
 
